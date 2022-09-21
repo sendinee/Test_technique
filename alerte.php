d9e3207de6091337df9fabd82143064b3b0c0182
Zo <zoe.marday@outlook.com>
2022-09-22 01:40:20 +0200

    Add files via upload

---
+++ alerte.php
@@ -0,0 +1,45 @@
+<?php
+    require_once('identifier.php');
+    
+    $message=isset($_GET['message'])?$_GET['message']:"Erreur";
+    
+    $url=isset($_GET['url'])?$_GET['url']:"index.php";
+    
+?>
+<! DOCTYPE HTML>
+<HTML>
+    <head>
+        <meta charset="utf-8">
+        <title>Alerte</title>
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../css/monstyle.css">
+    </head>
+    <body>
+        
+        <div class="container margetop60">
+           
+                <div class="alert alert-danger">
+                
+                    <h4><?php echo $message ?></h4> 
+                                      
+                </div>
+                
+                <br><br>
+                
+                <div class="alert alert-info">
+                
+                    <h4>Bonjour #NOM_ETUDIANT# #PRENOM_ETUDIANT#,<br>
+                        Vous avez suivi #nbHeur# de formation chez FormationPlus.
+                        Pouvez-vous nous retourner ce mail avec la pièce jointe signée.
+                        <br><br>
+                        Cordialement,<br>
+                        FormationPlus</h4>
+                    
+                   	<?php  header("refresh:5;url=$url"); ?>
+                   	
+                </div>
+        
+        </div>  
+            
+    </body>
+</HTML>
